599e60261a6a98cc586e8d6f4ad9b5f613ca0f15
Torstein Gjengedal <torstein.gjengedal@gmail.com>
2011-10-12 16:26:41 +0200

    Sets up config in spring applicationcontext

--- src/test/java/no/gjengeda/DynamicPropertyTest.java
+++ src/test/java/no/gjengeda/DynamicPropertyTest.java
@@ -2,18 +2,26 @@ package no.gjengeda;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/ApplicationContext.xml")
 public class DynamicPropertyTest {
 
     private static final Logger LOGGER = Logger.getLogger(DynamicPropertyTest.class);
 
+    @Autowired
+    private PropertiesConfiguration config;
+    
     @Test
     public void testDynamicPropertyLoading() throws ConfigurationException {
-        PropertiesConfiguration config = new PropertiesConfiguration("my.properties");
-        config.setReloadingStrategy(new FileChangedReloadingStrategy());
+        LOGGER.debug(config.getFileName());
         for (int i = 0; i < 2; i++) {
             LOGGER.debug(config.getString("myprop"));
         }
